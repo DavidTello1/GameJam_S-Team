d6ac7afa7ef189ef019abd6883d113392d67b8cd
Victor Chen <36187875+Scarzard@users.noreply.github.com>
2021-02-05 00:33:43 +0100

    ChessWin condition done. Just need to actually change scenes

---
+++ S-Team/Assets/Scripts/ChessWin.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChessWin : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float timer;
+    private float stayTime = 15.0f;
+    private bool start_timer = false;
+    // Update is called once per frame
+    void Update()
+    {
+        if (start_timer)
+            timer += Time.deltaTime;
+
+        if(timer > stayTime)
+        {
+            //Code to change to lvl2 here
+            Debug.Log("Change scene");
+            start_timer = false;
+        }
+          
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.layer == LayerMask.NameToLayer("MovableCube"))
+        {
+            start_timer = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.layer == LayerMask.NameToLayer("MovableCube"))
+            start_timer = false;
+    }
+}
