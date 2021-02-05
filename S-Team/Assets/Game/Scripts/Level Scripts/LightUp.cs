10e3c72e8c99815f220ba71b66e84d7917cf8259
Victor Chen <36187875+Scarzard@users.noreply.github.com>
2021-02-05 02:52:26 +0100

    Light strips added

---
+++ S-Team/Assets/Game/Scripts/Level Scripts/LightUp.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightUp : MonoBehaviour
+{
+    Renderer lightRenderer;
+    public Material baseMat;
+    public Material emissiveMat;
+    // Start is called before the first frame update
+    void Start()
+    {
+        lightRenderer = GetComponent<Renderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            lightRenderer.material = emissiveMat;
+            Debug.Log("Emissive");
+        }
+
+        if (Input.GetKeyDown(KeyCode.H))
+        {
+            Debug.Log("Base");
+            lightRenderer.material = baseMat;
+        }
+    }
+}
