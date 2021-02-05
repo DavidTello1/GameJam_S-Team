1367394dbded4c1152c72cc411fed5f783920e0e
ponspack9 <ospoga9@gmail.com>
2021-02-05 05:35:03 +0100

    LevelManager and FadeToblack

---
+++ S-Team/Assets/GameController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    static GameController instance;
+
+    private void Awake()
+    {
+        // Very first time game controller is instantiated
+        if (instance == null)
+        {
+            instance = this;
+            LevelManager.Init();
+            DontDestroyOnLoad(this);
+
+        }
+        else
+        {
+            Destroy(this);
+        }
+    }
+
+    public void MainMenuPlay(int level)
+    {
+        LevelManager.current_level = level;
+        LevelManager.LoadLevel();
+        Debug.Log("Entering level" + level.ToString());
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        LevelManager.Update();
+    }
+}
---
+++ S-Team/Assets/LevelManager.cs
@@ -0,0 +1,123 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public enum FadeState
+{
+    ToWhite,
+    ToBlack,
+
+    Idle
+}
+
+public static class LevelManager
+{
+    public static int current_level = 0;
+
+    // Transitioning
+    public static FadeState state = FadeState.Idle;
+    public static Image fadeToBlackImage;
+
+    private static float time = 0;
+    private static float transitionTime = 2.0f;
+    private static float timeStep = 0.0f;
+
+    static Color black0 = Color.black;
+    static Color black1 = Color.black;
+
+    public static void Init()
+    {
+        CreateFadeImage();
+        black0 = new Color(0, 0, 0, 0);
+        black1 = new Color(0, 0, 0, 1);
+
+        timeStep = 1.0f / transitionTime;
+
+        FadeToWhite();
+
+        SceneManager.sceneLoaded += OnSceneLoaded;
+
+    }
+
+    private static void CreateFadeImage()
+    {
+        if (fadeToBlackImage != null) return;
+        GameObject fadeToBlackCanvas = Resources.Load<GameObject>("FadeToBlackCanvas");
+        fadeToBlackImage = GameObject.Instantiate(fadeToBlackCanvas).GetComponentInChildren<Image>();
+    }
+
+    // Loads level "level" or Restarts the current level if not argument passed 
+    public static void LoadLevel()
+    {
+        FadeToBlack();
+    }
+    
+    
+    public static void Update()
+    {
+        time += timeStep * Time.deltaTime;
+
+        switch (state)
+        {
+            case FadeState.ToWhite:
+                fadeToBlackImage.color = Color.Lerp(black1, black0, time);
+                break;
+            case FadeState.ToBlack:
+                fadeToBlackImage.color = Color.Lerp(black0, black1, time);
+                break;
+            case FadeState.Idle:
+                break;
+        }
+
+        // Transition has finished
+        if (time > 1)
+        {
+            switch (state)
+            {
+                case FadeState.ToBlack:
+                    SceneManager.LoadScene(current_level);
+                    break;
+                default:
+                    state = FadeState.Idle;
+                    break;
+            }
+        }
+    }
+    public static void RestartLevel()
+    {
+        LoadLevel();
+    }
+
+    public static void NextLevel()
+    {
+        ++current_level;
+        LoadLevel();
+    }
+
+    public static void PreviousLevel()
+    {
+        --current_level;
+        LoadLevel();
+    }
+
+    public static void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        CreateFadeImage();
+        FadeToWhite();
+    }
+
+    static void FadeToBlack()
+    {
+        state = FadeState.ToBlack;
+        time = 0;
+    }
+
+    static void FadeToWhite()
+    {
+        state = FadeState.ToWhite;
+        time = 0;
+    }
+}
