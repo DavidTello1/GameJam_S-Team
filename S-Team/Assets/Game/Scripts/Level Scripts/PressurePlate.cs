e2f36c67e4050de968990827593dadf623b5e837
Victor Chen <36187875+Scarzard@users.noreply.github.com>
2021-02-05 12:53:11 +0100

    Pressure plate reacts when player size is big and new plate model

--- S-Team/Assets/Game/Scripts/Level Scripts/PressurePlate.cs
+++ S-Team/Assets/Game/Scripts/Level Scripts/PressurePlate.cs
@@ -7,13 +7,18 @@ public class PressurePlate : MonoBehaviour
     // Start is called before the first frame update
     public float timer;
     public bool pressed = false;
+    public PlayerScientist playerScience;
 
-    private float stayTime = 2.0f;
+    private float stayTime = 3.0f;
     private bool start_timer = false;
+    private bool found = false;
+
     // Update is called once per frame
     void Update()
     {
-        if (start_timer)
+        playerScience = GameObject.Find("ScienceFemale Variant(Clone)").GetComponent<PlayerScientist>();
+
+        if (start_timer && !pressed)
             timer += Time.deltaTime;
 
         if(timer > stayTime)
@@ -23,9 +28,9 @@ public class PressurePlate : MonoBehaviour
         }
     }
 
-    private void OnTriggerEnter(Collider other)
+    private void OnTriggerStay(Collider other)
     {
-        if (other.gameObject.CompareTag("Player"))
+        if (other.gameObject.CompareTag("Player") && playerScience.currentSize == 2.0f)
             start_timer = true;
     }
 
